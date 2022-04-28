3b323f1fca6a3db9b2a9e5ec01a4af49931233fe
moekidev <me@moeki.dev>
2022-04-29 02:30:12 +0900

    test analytics measurement protocol

--- api/download.js
+++ api/download.js
@@ -1,4 +1,16 @@
-export default function handler(request, response) {
-  const { name } = request.query;
-  response.status(200).send(`Hello ${name}!`);
+import ky from 'ky'
+
+export default async function handler(request, response) {
+  await ky.post('https://www.google-analytics.com?api_secret=SC-FSW2mQ9qfws_TjfuiJg&measurement_id=G-P50R59GSVY', {
+    body: JSON.stringify({
+      client_id: 'web',
+      events: [{
+        name: 'download',
+        params: {
+          filename: 'wakaran-ep12',
+          response: JSON.stringify(response)
+        }
+      }]
+    })
+  }).json()
 }
