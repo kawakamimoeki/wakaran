f5366cc5b6ca203d6bb45f408045cf02aa5f8c00
moekidev <me@moeki.dev>
2022-04-29 02:59:50 +0900

    set type module

--- api/download.js
+++ api/download.js
@@ -1,4 +1,4 @@
-const fetch = require('@vercel/fetch')(require('node-fetch'))
+import fetch from '@vercel/fetch'
 
 export default function handler(request, response) {
   fetch(`https://www.google-analytics.com?api_secret=${process.env.ANALYTICS_SECRET}&measurement_id=G-P50R59GSVY`, {
