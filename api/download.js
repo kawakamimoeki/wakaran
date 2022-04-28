a55a04b0cc5d4a282de35805b52143c84d95b267
moekidev <me@moeki.dev>
2022-04-29 03:28:57 +0900

    yarinaoshi

--- api/download.js
+++ api/download.js
@@ -1,19 +1,4 @@
-import fetch from 'node-fetch'
-
 export default async function handler(request, response) {
-  const res = await fetch(`https://www.google-analytics.com/mp/collect?api_secret=${process.env.ANALYTICS_SECRET}&measurement_id=G-P50R59GSVY`, {
-    method: 'POST',
-    body: JSON.stringify({
-      client_id: 'web',
-      events: [{
-        name: 'download',
-        params: {
-          filename: 'wakaran-ep12'
-        }
-      }]
-    })
-  })
-
-  const result = await res.text()
-  response.status(200).json(result)
+  console.log(request)
+  response.status(200).json('OK')
 }
